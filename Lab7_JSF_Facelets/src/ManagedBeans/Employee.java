a1326c34eee4102facd40c37c114271b3a203c55
RabinShrestha <lovingspirit617@gmail.com>
2017-12-18 01:10:29 -0600

    Template Changed

---
+++ Lab7_JSF_Facelets/src/ManagedBeans/Employee.java
@@ -0,0 +1,52 @@
+package ManagedBeans;
+
+public class Employee
+    {
+        String name;
+        String id;
+        String phone;
+        Double salary;
+        public Employee()
+        {}
+        public Employee(String id, String name, String phone,Double salary)
+        {
+            this.id=id;
+            this.name=name;
+            this.phone=phone;
+            this.salary=salary;
+        }
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
+
+        public void setPhone(String phone) {
+            this.phone = phone;
+        }
+
+        public Double getSalary() {
+            return salary;
+        }
+
+        public void setSalary(Double salary) {
+            this.salary = salary;
+        }
+
+
+
+    }
--- Lab7_JSF_Facelets/src/ManagedBeans/EmployeeDetails.java
+++ Lab7_JSF_Facelets/src/ManagedBeans/EmployeeDetails.java
@@ -2,68 +2,60 @@ package ManagedBeans;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Rabin Shrestha on 12/15/2017.
  */
-@ManagedBean
+@ManagedBean(name = "employeeDetails")
 @RequestScoped
-public class EmployeDetails {
+public class EmployeeDetails {
 
-    List<Employee> employeeList=new ArrayList<>();
-    public EmployeDetails()
+
+
+    private List<Employee> employeeList;
+    Employee employee;
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public EmployeeDetails() {
+
+        employeeList = new ArrayList<>();
+        employeeList.add(new Employee("1432", "Frank Brown", "625-345-2635", 45089.5));
+        employeeList.add(new Employee("1321", "John Doe", "625-345-3321", 56003.0));
+        employeeList.add(new Employee("1634", "Mary Jones", "625-345-3421", 73998.3));
+    }
+
+    public List<Employee> getEmployeeList() {
+        return employeeList;
+    }
+
+    public Employee getEmployee()
     {
+        Map<String, String> params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        String employeeId = params.get("eid");
+        System.out.println("The Selected Employee ID : "+ employeeId);
+        for (Employee employee : employeeList) {
+            if (employee.getId().equals(employeeId)) {
+                System.out.println("The Selected Employee Details");
+                System.out.println(employee.getId());
+                System.out.println(employee.getName());
+                System.out.println(employee.getSalary());
+                this.employee=employee;
+                return employee;
+            }
+        }
+        this.employee=employeeList.get(0);
 
+        return employeeList.get(0);
     }
 
 
 }
 
 
-class Employee
-{
-    String name;
-    String id;
-    String phone;
-    Double salary;
-    public Employee()
-    {}
-    public Employee()
-    {}
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public Double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Double salary) {
-        this.salary = salary;
-    }
-
-
-
-}
